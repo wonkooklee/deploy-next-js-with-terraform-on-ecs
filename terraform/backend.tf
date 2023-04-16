91d900d22c67aedfdea73fdb589764673b7e137d
Wonkook Lee <wonkook.lee@medistream.com>
2023-04-17 00:17:04 +0900

    fix: backend 구축용 S3, DynamoDB 프로비저닝 코드 제거

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,36 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "julu-alpha-bravo-alpine200392"
-    key            = "terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
-
-resource "aws_s3_bucket" "tfstate" {
-  bucket = var.backend_bucket_name
-}
-
-resource "aws_s3_bucket_acl" "tfstate_acl" {
-  bucket = aws_s3_bucket.tfstate.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "tfstate_versioning" {
-  bucket = aws_s3_bucket.tfstate.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-lock"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
+    bucket  = "s3-bucket-tfstate-2308ru2308fuwe"
+    key     = "terraform.tfstate"
+    region  = "ap-northeast-2"
+    encrypt = true
   }
 }
